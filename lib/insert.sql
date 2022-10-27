be3dbb66c591069b4211081920ee0d580e2b3305
Mercy <mercycheruiyot2013@gmail.com>
2022-10-28 02:58:35 +0300

    Completed assignement

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)VALUES("Mr.Chocolate",5,"F","dark",'Happy',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy",6,"M","brown","sad",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha",7,"F","yellow","moody",false);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergent Brown",7,"M","black","moody",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa",8, "F","brown","moody",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch",9,"F","black","jovial",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy",10,"F","brown","jovial",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,10,"F","brown","jovial",true);
